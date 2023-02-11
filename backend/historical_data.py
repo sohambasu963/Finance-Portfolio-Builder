42abb7908ebb0323bcf88d32b082623ebdcce0f6
Soham Basu <sohambasu963@gmail.com>
2023-02-11 12:02:16 -0500

    added changes

--- backend/historical_data.py
+++ backend/historical_data.py
@@ -32,7 +32,7 @@ def get_performance(df, symbol, ticker):
 
 
 def get_historical_data(df, symbol, ticker):
-    start_date = "2008-01-01"
+    start_date = "2013-01-01"
     end_date = "2018-01-01"
 
     historical_data = ticker.history(start=start_date, end=end_date)
@@ -44,8 +44,6 @@ def get_historical_data(df, symbol, ticker):
         df.loc[symbol]['div_yield'] = info['dividendYield']
     if 'beta' in info.keys():
         df.loc[symbol]['beta'] = info['beta']
-    if 'targetMeanPrice' in info.keys():
-        df.loc[symbol]['analyst_target'] = info['targetMeanPrice']
 
     daily_returns = historical_data['Close'].pct_change()
     df.loc[symbol]['avg_return'] = daily_returns.mean() * 252
@@ -53,34 +51,41 @@ def get_historical_data(df, symbol, ticker):
     df.loc[symbol]['price'] = round(historical_data['Close'][-1], 2)
 
 def store_data():
+    rf_rate = yf.Ticker("^TNX").history(period="1y")['Close'].mean() / 100
+
     table = pd.read_html(SP500_DATA_URL)
     df = table[0].loc[:, ['Symbol']].set_index('Symbol')
     df_len = len(df)
 
-    rf_rate = yf.Ticker("^TNX").history(period="1y")['Close'].mean() / 100
-
     df['avg_return'] = [None] * df_len
     df['volatility'] = [None] * df_len
     df['sharpe_ratio'] = [None] * df_len
-
     df['price'] = [None] * df_len
     df['forward_pe'] = [None] * df_len
     df['div_yield'] = [None] * df_len
     df['beta'] = [None] * df_len
-    df['analyst_target'] = [None] * df_len
-
     stock_symbols = df.index.tolist()
+
+    performance_df = table[0].loc[:, ['Symbol']].set_index('Symbol')
+    performance_df['total_return'] = [None] * df_len
+    performance_df['avg_return'] = [None] * df_len
+    performance_df['volatility'] = [None] * df_len
+    performance_df['sharpe_ratio'] = [None] * df_len
+
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
         for symbol in stock_symbols:
             executor.submit(get_historical_data, df, symbol, yf.Ticker(symbol.replace(".", "-")))
-            executor.submit(get_performance, df, symbol, yf.Ticker(symbol.replace(".", "-")))
+            executor.submit(get_performance, performance_df, symbol, yf.Ticker(symbol.replace(".", "-")))
 
     df['beta'].fillna(1, inplace=True)
     df.fillna(0, inplace=True)
     df['sharpe_ratio'] = (df['avg_return'] - rf_rate) / df['volatility']
+    df.to_csv("./data/" + "stock_data" + ".csv")
 
-    df.to_csv("./data/" + "old_stock_data" + ".csv")
+    # remove rows with missing data
+    performance_df.fillna(0, inplace=True)
+    performance_df.to_csv("./data/" + "performance_data" + ".csv")
 
 def print_time():
     end_time = time.time()
