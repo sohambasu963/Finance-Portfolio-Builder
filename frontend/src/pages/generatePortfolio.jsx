fad574e584c7c66649b0f595878d4202b0a530b9
Soham Basu <sohambasu963@gmail.com>
2023-02-13 00:21:38 -0500

    added frontend pages

---
+++ frontend/src/pages/generatePortfolio.jsx
@@ -0,0 +1,9 @@
+export default function Investment() {
+    return (
+      <>
+        <div>
+          <h1>Generate a portfolio here</h1>
+        </div>
+      </>
+    )
+  }
---
+++ frontend/src/pages/searchInvestment.jsx
@@ -0,0 +1,9 @@
+export default function Investment() {
+    return (
+      <>
+        <div>
+          <h1>Search for investments here</h1>
+        </div>
+      </>
+    )
+  }
