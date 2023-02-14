c6b502fb5ffbde7b3f352aa83cf3cf82e5465b55
Soham Basu <sohambasu963@gmail.com>
2023-02-13 23:55:14 -0500

    made css changes

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,12 +1,19 @@
 import Link from 'next/link'
+import Head from 'next/head'
+// import 'bootstrap/dist/css/bootsrap.min.css'
+// import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
-      <div>
-        <h1 className='head'>Stock Watchlist</h1>
-
-      </div>
+      <Head>
+        <title>Investor Aid</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"></link>
+      </Head>
+      <h1>Stock Watchlist</h1>
+      {/* <div className={styles.container}>
+        <h1 className={styles.title}>Stock Watchlist</h1>
+      </div> */}
     </>
   )
 }
