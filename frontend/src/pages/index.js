b061ffb32900dcc87e40b4b876c4f31ff617800f
Soham Basu <sohambasu963@gmail.com>
2023-02-20 14:03:13 -0500

    added small frontend change

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,19 +1,40 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import { useState, useEffect } from 'react'
 // import 'bootstrap/dist/css/bootsrap.min.css'
 // import styles from '@/styles/Home.module.css'
 
 export default function Home() {
+
+  const [search, setSearch] = useState('')
+
+  function handleSearch(e) {
+    e.preventDefault();
+    setSearch(e.target.value);
+  }
+
+  useEffect(() => {
+    console.log(search)
+  }, [search])
+
   return (
     <>
       <Head>
         <title>Investor Aid</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"></link>
       </Head>
-      <h1>Stock Watchlist</h1>
-      {/* <div className={styles.container}>
-        <h1 className={styles.title}>Stock Watchlist</h1>
-      </div> */}
+      <div className="header">
+        <h1>Stock Watchlist</h1>
+        <p>Search for stocks below to add them to your watchlist</p>
+      </div>
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Search for a stock"
+          value={search}
+          onChange={handleSearch}
+        ></input>
+      </div>
     </>
   )
 }
