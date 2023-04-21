eedb68fbc818cd8c45d637ab9d09041c131d8644
Soham Basu <sohambasu963@gmail.com>
2023-04-21 15:47:45 -0400

    revamped frontend search bar

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchBar from './components/SearchBar';
+import { useState } from 'react';
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+
+  const handleSearch = (searchTerm) => {
+    return ['AAPL', 'GOOGL', 'MSFT'];
+  };
+
+  const handleSuggestionSelect = (suggestion) => {
+    console.log(suggestion)
+    setSearchTerm(suggestion);
+    // Handle the suggestion selection, e.g., fetch stock data
+  };
+
+  return (
+    <div className="App">
+      <div className='dashboard'>
+        <h1>Stock Watchlist</h1>
+        <p className='subheading'>Search for stocks below to add them to your watchlist</p>
+        <SearchBar
+        onSearch={handleSearch}
+        onSuggestionSelect={handleSuggestionSelect}
+      />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,69 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { Form, InputGroup, Dropdown } from 'react-bootstrap';
+import { Search } from 'react-bootstrap-icons';
+
+const SearchBar = ({ onSearch, onSuggestionSelect }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const searchBarRef = useRef(null);
+
+  const handleSearch = (e) => {
+    const term = e.target.value;
+    setSearchTerm(term);
+    
+    if (term.trim() === '') {
+      setSuggestions([]);
+    } else {
+      const newSuggestions = onSearch(term);
+      setSuggestions(newSuggestions || []);
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (searchBarRef.current && !searchBarRef.current.contains(event.target)) {
+        setSearchTerm('');
+        setSuggestions([]);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div ref={searchBarRef}>
+      <Form onSubmit={(e) => e.preventDefault()}>
+        <InputGroup size="lg">
+          <Form.Control
+            type="text"
+            placeholder="Search for stocks..."
+            value={searchTerm}
+            onChange={handleSearch}
+          />
+          <InputGroup.Text>
+            <Search />
+          </InputGroup.Text>
+        </InputGroup>
+        {suggestions.length > 0 && (
+          <Dropdown.Menu show>
+            {suggestions.map((suggestion, index) => (
+              <Dropdown.Item
+                key={index}
+                onSelect={() => {
+                  onSuggestionSelect(suggestion);
+                  setSuggestions([]);
+                }}
+              >
+                {suggestion}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        )}
+      </Form>
+    </div>
+  );
+};
+
+export default SearchBar;
