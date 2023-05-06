77503d6d93eef052ac786d4c36cf9a473453f62a
Soham Basu <sohambasu963@gmail.com>
2023-05-06 12:34:34 -0400

    added new file and moved alpha api calls to python

---
+++ backend/alpha_data.py
@@ -0,0 +1,76 @@
+import os
+import requests
+import pandas as pd
+import numpy as np
+from alpha_vantage.timeseries import TimeSeries
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from concurrent.futures import ThreadPoolExecutor
+import time
+
+load_dotenv(".env.local")
+
+ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
+SP500_DATA_URL = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+TSX60_DATA_URL = 'https://en.wikipedia.org/wiki/S%26P/TSX_60'
+
+def scrape_tickers(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    tickers = []
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text.strip().replace(".", "-")
+        tickers.append(ticker)
+    return tickers
+
+def get_stock_data(ticker, interval='daily', outputsize='full'):
+    ts = TimeSeries(key=ALPHA_VANTAGE_API_KEY, output_format='pandas')
+    if interval == 'daily':
+        data, _ = ts.get_daily_adjusted(symbol=ticker, outputsize=outputsize)
+    elif interval == 'weekly':
+        data, _ = ts.get_weekly_adjusted(symbol=ticker)
+    elif interval == 'monthly':
+        data, _ = ts.get_monthly_adjusted(symbol=ticker)
+    return data
+
+def calculate_metrics(ticker_data):
+    daily_returns = ticker_data['5. adjusted close'].pct_change().dropna()
+    avg_return = daily_returns.mean()
+    volatility = daily_returns.std()
+    sharpe_ratio = avg_return / volatility
+    return avg_return, volatility, sharpe_ratio
+
+def get_stock_metrics(ticker):
+    try:
+        ticker_data = get_stock_data(ticker)
+        avg_return, volatility, sharpe_ratio = calculate_metrics(ticker_data)
+        time.sleep(1.1)
+        return {
+            'Ticker': ticker,
+            'Average Return': avg_return,
+            'Volatility': volatility,
+            'Sharpe Ratio': sharpe_ratio,
+        }
+    except Exception as e:
+        print(f"Error fetching data for {ticker}: {e}")
+
+def main():
+    sp500_tickers = scrape_tickers(SP500_DATA_URL)
+    tsx60_tickers = scrape_tickers(TSX60_DATA_URL)
+    all_tickers = sp500_tickers + tsx60_tickers
+
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        stock_metrics = list(executor.map(get_stock_metrics, all_tickers))
+
+    stock_metrics = [metric for metric in stock_metrics if metric is not None]
+    stock_metrics_df = pd.DataFrame(stock_metrics)
+    stock_metrics_df.to_csv('../data/stock_metrics.csv', index=False)
+    return stock_metrics_df
+
+if __name__ == "__main__":
+    start = time.time()
+    result_df = main()
+    print(result_df)
+    print(f"Time taken: {time.time() - start} seconds")
+
--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,105 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from calculate_portfolio import get_portfolio
+from dotenv import load_dotenv
+import os
+import requests
 
 app = Flask(__name__)
 CORS(app)
 
-# @app.route('/')
-# def home():
-#     return jsonify({"message": "Hello, this is a simple endpoint!"})
+load_dotenv(".env.local")
+ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
+API_URL = "https://www.alphavantage.co/query"
 
 @app.route('/portfolio', methods=['POST'])
 def calculate_portfolio():
     stocks = request.json
     data = get_portfolio(stocks)
     return jsonify(data)
+
+@app.route('/search', methods=['GET'])
+def search(search_term):
+    function = 'SYMBOL_SEARCH'
+
+    response = requests.get(
+        f"{API_URL}?function={function}&keywords={search_term}&apikey={ALPHA_VANTAGE_API_KEY}"
+    )
+    data = response.json()
+
+    if "bestMatches" not in data:
+        print("Invalid API response:", data)
+        return []
+
+    # Extract stock suggestions from the API response
+    suggestions = [
+        {
+            "symbol": item["1. symbol"],
+            "name": item["2. name"],
+            "currency": item["8. currency"],
+        }
+        for item in data["bestMatches"]
+        if item["8. currency"] in ["USD", "CAD"]
+    ]
+
+    return jsonify(suggestions)
+
+
+@app.route('/quote', methods=['GET'])
+def get_quote(suggestion):
+    quote_function = 'GLOBAL_QUOTE'
+    overview_function = 'OVERVIEW'
+
+    quote_response = requests.get(
+        f"{API_URL}?function={quote_function}&symbol={suggestion['symbol']}&apikey={ALPHA_VANTAGE_API_KEY}"
+    )
+    quote_data = quote_response.json()
+
+    overview_response = requests.get(
+        f"{API_URL}?function={overview_function}&symbol={suggestion['symbol'].split('.')[0]}&apikey={ALPHA_VANTAGE_API_KEY}"
+    )
+    overview_data = overview_response.json()
+
+    stock_data = {
+        "symbol": suggestion["symbol"],
+        "name": suggestion["name"],
+        "price": round(float(quote_data["Global Quote"]["05. price"]), 2),
+        "currency": suggestion["currency"],
+        "percentChange": round(float(quote_data["Global Quote"]["10. change percent"].rstrip("%")), 2),
+        "dividendYield": round(float(overview_data["DividendYield"]) * 100, 2) if overview_data["DividendYield"] else "N/A",
+        "peRatio": round(float(overview_data["PERatio"]), 2) if overview_data["PERatio"] and overview_data["PERatio"] != "None" else "N/A",
+        "beta": round(float(overview_data["Beta"]), 2) if overview_data["Beta"] else "N/A",
+    }
+
+    return jsonify(stock_data)
+
+
+@app.route('/performance', methods=['GET'])
+def get_performance_data(symbol):
+    function = 'TIME_SERIES_DAILY_ADJUSTED'
+
+    response = requests.get(
+        f"{API_URL}?function={function}&symbol={symbol}&outputsize=compact&apikey={ALPHA_VANTAGE_API_KEY}"
+    )
+    data = response.json()
+
+    if "Time Series (Daily)" not in data:
+        print("Invalid API response:", data)
+        return []
+
+    time_series_data = data["Time Series (Daily)"]
+
+    dates = list(time_series_data.keys())
+    adjusted_closing_prices = [round(float(time_series_data[date]["5. adjusted close"]), 2) for date in dates]
+
+    performance_data = {
+        "stock": data["Meta Data"]["2. Symbol"],
+        "labels": dates,
+        "data": adjusted_closing_prices
+    }
+
+    return jsonify(performance_data)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
